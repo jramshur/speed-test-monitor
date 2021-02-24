a05ea06f5dae4da295212d1bd911be0f436cdb9e
John Ramshur <jramshur@users.noreply.github.com>
2021-02-23 23:57:06 -0600

    clean some code and refine 30min speed test interval
    Fixed #6 .

--- main.py
+++ main.py
@@ -12,15 +12,11 @@ def get_new_speeds():
 
     # Get ping (miliseconds)
     ping = speed_test.results.ping
-    # Perform download and upload speed tests (bits per second)
-    download = speed_test.download()
-    upload = speed_test.upload()
+    # Perform download and upload speed tests, convert to Mbits per second, and round to 2 decimal points
+    download = round(speed_test.download()/(10**6), 2)
+    upload = round(speed_test.upload()/(10**6), 2)
 
-    # Convert download and upload speeds to megabits per second
-    download_mbs = round(download / (10**6), 2)
-    upload_mbs = round(upload / (10**6), 2)
-
-    return (upload_mbs, download_mbs, ping)
+    return (upload, download, ping)
 
 # wait 60 seconds to allow Pi to reboot fully before running speed test. This assumes this script will be run at boot.
 time.sleep(60)
@@ -39,4 +35,4 @@ while True:
     payload = {'SheetName':SHEETNAME,'DeviceID':DEVICEID, 'TimeStamp':dt,'UpSpeed':speeds[0],'DownSpeed':speeds[1],'Ping':speeds[2]}
     response =  requests.get(url, params=payload) #Push speed test results
     print("Pushing Data: ", response.url)
-    time.sleep(30*60) # wait 30 mintues between speed test
+    time.sleep((30*60) - 30) # wait 30 mintues between speed test (30 min x 60 s/min - 30s offset) offset is to count for time for code to run.
